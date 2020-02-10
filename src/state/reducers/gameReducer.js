4bc7563c390282278a68501fa3cb5aba6bf526e4
Ryan Pays <ryan.pays@gmail.com>
2020-02-10 13:19:04 +0000

    refactor reducer

--- src/state/reducers/gameReducer.js
+++ src/state/reducers/gameReducer.js
@@ -9,62 +9,58 @@ export const initialState = {
   ]
 };
 
+function updateObject(oldObject, newValues) {
+  return Object.assign({}, oldObject, newValues);
+}
+
+function updateItemInArray(array, itemId, updateItemCallback) {
+  const updatedItems = array.map(item => {
+    if (item.id !== itemId) {
+      return item;
+    }
+    const updatedItem = updateItemCallback(item);
+    return updatedItem;
+  });
+  return updatedItems;
+}
+
 export const gameReducer = (state = initialState, action = "") => {
+  let newPlayers;
+
   switch (action.type) {
     case ACTION_TYPES.INCREMENT_GAME_COUNT:
-      return {
-        ...state,
-        gameCounter: state.gameCounter + 1
-      };
+      return updateObject(state, { gameCounter: state.gameCounter + 1 });
     case ACTION_TYPES.SET_SELECTED_RESOURCE:
-      return {
-        ...state,
-        selectedResource: action.payload
-      };
+      return updateObject(state, { selectedResource: action.payload });
     case ACTION_TYPES.INCREMENT_WIN_COUNT_BY_PLAYER_ID:
-      return {
-        ...state,
-        players: [...state.players].map(player => {
-          return player.id !== action.payload
-            ? player
-            : {
-                ...player,
-                winCount: player.winCount + 1
-              };
-        })
-      };
+      newPlayers = updateItemInArray(state.players, action.payload, player => {
+        return updateObject(player, { winCount: player.winCount + 1 });
+      });
+      return updateObject(state, { players: newPlayers });
     case ACTION_TYPES.SET_TOP_TRUMP_BY_PLAYER_ID:
-      return {
-        ...state,
-        players: [...state.players].map(player => {
-          return player.id !== action.payload.id
-            ? player
-            : {
-                ...player,
-                ...action.payload
-              };
-        })
-      };
+      newPlayers = updateItemInArray(
+        state.players,
+        action.payload.id,
+        player => {
+          return updateObject(player, { topTrump: action.payload.topTrump });
+        }
+      );
+      return updateObject(state, { players: newPlayers });
     case ACTION_TYPES.SET_WINNING_ATTRIBUTE_BY_PLAYER_ID:
       return {
         ...state,
         players: [...state.players].map(player => {
-          return player.id !== action.payload.id
-            ? {
-                ...player,
-                attribute: ""
-              }
-            : {
-                ...player,
-                attribute: action.payload.attribute
-              };
+          return updateObject(player, {
+            attribute:
+              player.id !== action.payload.id ? "" : action.payload.attribute
+          });
         })
       };
     case ACTION_TYPES.RESET_WINNING_ATTRIBUTE:
       return {
         ...state,
         players: [...state.players].map(player => {
-          return { ...player, ...action.payload };
+          return updateObject(player, { ...action.payload });
         })
       };
     default:
